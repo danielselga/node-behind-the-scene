34a1d1c3221df2f0f356f3ff307ed7a78c5cce28
Daniel <danielselga@gmail.com>
2021-09-02 11:44:54 -0300

    chore: ndoe event-loop order

---
+++ event-loop.js
@@ -0,0 +1,26 @@
+const fs = require('fs') //Modules require
+
+setTimeout(() => { //Will execute frist because is outside a callback
+    console.log('Timer 1 finished'), 0
+})
+
+setImmediate(() => { //Will execute frist because is outside a callback
+    console.log('Imediate 1 finished')
+})
+
+fs.readFile('test-file.txt', () => {
+    console.log('I/0 Finished') // Frist of event-loop
+    
+    setImmediate(() => { // Second in the chain
+        console.log('Imediate 2 finished')
+    })
+    
+    setTimeout(() => { // Last one, and the order is based on timer.
+        console.log('Timer 2 finished')
+    }, 3000)
+
+
+    process.nextTick(() => console.log('process.nextick')) //Frist callback wich will be executed 
+})
+
+console.log('Hello from top-level code!') //Frist of all because is a top-level-code
